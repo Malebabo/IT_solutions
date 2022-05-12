0d9d6556d360f19ed7b9c8cac7254159fd7c5588
Malebabo <malebabomoorosi@gmail.com>
2022-05-12 23:52:39 +0200

    exercise 4

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Button, TouchableOpacity, TextInput } from 'react-native';
+import { Component } from 'react';
+import Login from './Login';
+import SignUp from './SignUp';
+import { useState } from 'react';
 
 export default function App() {
+  const [Registered, setRegistered] = useState(true);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <ImageBackground style = {{height: 270, width: '100%',marginTop: 28}}
+        source = {require('./assets/wall.jpg')}>
+           <Text style = {{fontSize: 27,color:'#D4D4D4',marginTop: 70,marginLeft:30}}>Welcome</Text>
+           <Text style = {{fontSize: 12,color:'#D4D4D4',marginLeft:30,marginBottom: 100}}>
+             Business IT Solutions and Innovation, Inc.</Text>
+              {Registered? <Login/>:<SignUp setRegistered = {setRegistered}/>}
+       </ImageBackground>
+      
+       
     </View>
   );
 }
@@ -13,8 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D4D4D8',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
---
+++ Login.js
@@ -0,0 +1,16 @@
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+export default function Login() {
+  return (
+     <View style = {styles.container}>
+     </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#fff'
+    }
+});
